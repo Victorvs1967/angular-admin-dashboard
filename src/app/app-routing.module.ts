8a74560a6ffd17661661ea72b5bd4879c9d790b1
Victor <victorsmirnov@me.com>
2022-05-22 19:54:18 +0300

    edit path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { AdminGuard } from './guard/admin.guard';
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard], canDeactivate: [AuthGuard] },
   { path: "auth", loadChildren: () => import('./auth/auth.module').then(M => M.AuthModule) },
-  { path: "admin", loadChildren: () => import('./dasboard/dashboard.module').then(M => M.DashboardModule) },
+  { path: "admin", canActivate: [AuthGuard], canActivateChild: [AdminGuard], loadChildren: () => import('./dasboard/dashboard.module').then(M => M.DashboardModule) },
 ];
 
 @NgModule({
--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -14,7 +14,7 @@ export class AuthGuard implements CanActivate, CanDeactivate<unknown> {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (!this.auth.onLogin()) {
-      this.router.navigate(['login']);
+      this.router.navigate(['/auth/login']);
       return false;
     }
     return true;
